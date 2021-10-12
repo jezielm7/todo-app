656d3414bc9bb2cba112073f3d0aaba9b7d12ab1
jeziel <jezielmarques7@gmail.com>
2021-10-12 01:33:39 -0300

    update screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Text, Platform, KeyboardAvoidingView } from 'react-native';
 
-import { Text } from 'react-native';
+import {
+  Container,
+  Form,
+  Input,
+  Button,
+  ButtonText,
+  ListView,
+  List,
+  Card,
+  TaskTitle,
+  TrashButton,
+} from './styles';
 
-import { Container } from './styles';
+import Data from '../../utils/data';
+
+interface Task {
+  id: string;
+  title: string;
+}
 
 function Home() {
-  return (
-    <Container>
-      <Text>Home</Text>
-    </Container>
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    setTasks(Data);
+  }, []);
+
+  const renderItem = ({ item }: { item: Task }) => (
+    <Card>
+      <TaskTitle>{item.title}</TaskTitle>
+      <TrashButton>
+        <Text>🗑</Text>
+      </TrashButton>
+    </Card>
   );
-};
+
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <Container>
+        <ListView>
+          <List
+            data={tasks}
+            renderItem={renderItem}
+            keyExtractor={item => `${item.id}`}
+            showsVerticalScrollIndicator={false}
+          />
+        </ListView>
+        <Form>
+          <Input
+            maxLength={25}
+            value={newTask}
+            onChangeText={task => setNewTask(task)}
+            autoCorrect={false}
+            placeholderTextColor="#999"
+            placeholder="Adicione uma tarefa"
+          />
+          <Button>
+            <ButtonText>+</ButtonText>
+          </Button>
+        </Form>
+      </Container>
+    </KeyboardAvoidingView>
+  );
+}
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,4 +2,75 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  padding: 20px;
+  margin-top: 20px;
+  background-color: #fff;
 `;
+
+export const Form = styled.View`
+  padding: 0;
+  height: 60px;
+  margin-top: 13px;
+
+  flex-direction: row;
+  align-self: stretch;
+  justify-content: center;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  height: 50px;
+  padding: 5px 10px;
+  border-radius: 8px;
+
+  background-color: #eee;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 50px;
+  height: 50px;
+  margin-left: 10px;
+  border-radius: 8px;
+
+  align-items: center;
+  justify-content: center;
+  background-color: #ff2748;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const ListView = styled.View`
+  flex: 1;
+`;
+
+export const List = styled.FlatList`
+  flex: 1;
+  margin-left: 5px;
+`;
+
+export const Card = styled.View`
+  padding: 15px;
+  border-radius: 8px;
+  margin-bottom: 15px;
+
+  flex-direction: row;
+  background-color: #eee;
+
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TaskTitle = styled.Text`
+  color: #333;
+  margin-top: 4px;
+  font-size: 14px;
+
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const TrashButton = styled.TouchableOpacity``;
